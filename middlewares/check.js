048a333699b8ee68a3bc1696d491a64949612c16
JIANQIN GAO <jianqingao@nwk-26-209.nwk.ruw.rutgers.edu>
2017-04-25 15:53:36 -0400

    add the login status check middleware

---
+++ middlewares/check.js
@@ -0,0 +1,16 @@
+module.exports = {
+    checkLogin: function checkLogin(req, res, next) {
+        if (!req.session.user) {
+            req.flash('error', 'You need to log in!');
+            return res.redirect('/signin');
+        }
+        next();
+    },
+    checkNotLogin: function checkNotLogin(req, res, next) {
+        if (req.session.user) {
+            req.flash('error', 'You have already signed in!');
+            return res.redirect('back');
+        }
+        next();
+    }
+}
